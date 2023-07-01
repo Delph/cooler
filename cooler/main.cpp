56ea0f2ebaf6703df5228b6e684207fce9b2ec39
Delph <49454418+Delph@users.noreply.github.com>
2023-07-01 23:19:05 +0100

    fixed logic issues rewrote cooler module control for better balancing new temperature thesholds implemented interlock (commented out) tidied up some bits

--- cooler/main.cpp
+++ cooler/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 
+
+#include <avr/wdt.h>
+
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -8,21 +12,23 @@
 #define DEBUG 1
 
 // pin assignments
-const uint8_t PIN_COOLERS[] = {2, 3, 4, 5, 6, 7, 8, 9};
-const size_t NUM_COOLERS = sizeof(PIN_COOLERS) / sizeof(uint8_t);
-const uint8_t PIN_SENSOR = 10;
+// the coolers are wired in order, 1-4 down the left, 5-8 down the right
+// order them here left-to-right, top-to-bottom for better thermal management
+const uint8_t PIN_COOLERS[] = {2, 6, 3, 7, 4, 8, 5, 9};
+const uint8_t PIN_TEMPERATURE_SENSOR = 10;
 const uint8_t PIN_ALARM = 11;
+// const uint8_t PIN_INTERLOCK = 22;
 
-// used to tell the watchdog arduino we're still running
+// led inbuilt, just flash it every so often
 const uint8_t PIN_WATCH = 13;
 
+// used for seeding the rng to randomise the starting index
+const uint8_t PIN_SEED = A0;
+
+const size_t NUM_COOLERS = sizeof(PIN_COOLERS) / sizeof(uint8_t);
 
 // settings
-// this is how often PIN_WATCH is toggled
-const uint32_t PULSE_RATE = 3000;
-
 // how often system logic runs to check temperatures etc
-// independant of PULSE RATE
 const uint32_t TICK_RATE = 500;
 
 // purge time settings
@@ -33,27 +39,41 @@ const uint32_t PURGE_TIME = 10 * 1000;
 
 // the minimum temperature the system will cool to
 // in degrees celsius
-const float TEMP_MIN = 18.0f;
+const float TEMPERATURE_MINIMUM = 16.0f;
+
+// the maximum temperature the system will attempt to maintain
+// above this temperature, all cooling elements will be on
+const float TEMPERATURE_MAXIMUM = 18.0f;
+
+// the temperature at which to trigger the alarm
+const float TEMPERATURE_ALARM_LOW = 14.0f;
+const float TEMPERATURE_ALARM_HIGH = 19.0f;
+
+// the temperature at which we arm the alarm
+const float TEMPERATURE_ARM = 17.0f;
 
 // how much the temperature goes up for each level
-const float TEMP_INCREMENT = 0.25f;
+const float TEMPERATURE_INCREMENT = (TEMPERATURE_MAXIMUM - TEMPERATURE_MINIMUM) / static_cast<float>(NUM_COOLERS);
 
 // hysteresis in temperature
 // prevents rapid toggling of cooler pads
-const float TEMP_HYSTERESIS = 0.125f;
+const float TEMPERATURE_HYSTERESIS = TEMPERATURE_INCREMENT / 2.0f;
 
-// the temperature at which to trigger the alarm
-const float TEMP_ALARM = 24.0f;
-
-// the temperature at which we arm the alarm
-const float TEMP_ARM = 19.0f;
 
 // variables
 // cooling level -- a la number of coolers on
 size_t level = 0;
 
-// time when cooler was last on -- for purge
+// this is the next cooler to turn on
+size_t head = 0;
+
+// this is the next cooler to turn off
+size_t tail = 0;
+
+// time when cooler was last on
 uint32_t lastOn[NUM_COOLERS] = {0};
+// time when cooler was last off
+uint32_t lastOff[NUM_COOLERS] = {0};
 
 // current cooler on purge, if == NUM_COOLERS then none
 size_t purge = NUM_COOLERS;
@@ -61,9 +81,11 @@ size_t purge = NUM_COOLERS;
 // if true, the temperature has decreased enough for us to arm
 bool armed = false;
 
+
 // temperature sensor
-OneWire oneWire(PIN_SENSOR);
-DallasTemperature sensor(&oneWire);
+OneWire oneWire(PIN_TEMPERATURE_SENSOR);
+DallasTemperature temperatureSensor(&oneWire);
+
 
 void setup()
 {
@@ -75,15 +97,45 @@ void setup()
 
   for (size_t i = 0; i < NUM_COOLERS; ++i)
     pinMode(PIN_COOLERS[i], OUTPUT);
-  pinMode(PIN_SENSOR, INPUT);
+  pinMode(PIN_TEMPERATURE_SENSOR, INPUT);
   pinMode(PIN_ALARM, OUTPUT);
+  // pinMode(PIN_INTERLOCK, OUTPUT);
+
+  randomSeed(analogRead(A0));
+
+  // start on a random head/tail
+  head = tail = random(0, NUM_COOLERS);
+
+  // turn off the interlock
+  // digitalWrite(PIN_INTERLOCK, false);
 
   // start all off
   for (size_t i = 0; i < NUM_COOLERS; ++i)
     digitalWrite(PIN_COOLERS[i], 1);
 
-  sensor.begin();
+  // begin the temperature sensor
+  temperatureSensor.begin();
+
+  // enable the watchdog timer at 4s internals, which should be plenty
+  wdt_enable(WDTO_4S);
 }
+
+
+void turnCoolerOn(const uint32_t now)
+{
+  digitalWrite(PIN_COOLERS[head], 0);
+  lastOn[head] = now;
+  head = (head + 1) % NUM_COOLERS;
+}
+
+
+void turnCoolerOff(const uint32_t now)
+{
+  digitalWrite(PIN_COOLERS[tail], 0);
+  lastOff[tail] = now;
+  tail = (tail + 1) % NUM_COOLERS;
+}
+
 
 void loop()
 {
@@ -93,82 +145,93 @@ void loop()
   // toggle the watch pin
   digitalWrite(PIN_WATCH, (now / 1000) % 2);
 
+  // reset the watchdog timer
+  wdt_reset();
+
   // check if it's time
-  if (next < now)
+  if (now < next)
     return;
   next = now + TICK_RATE;
 
   // do logic
-  sensor.requestTemperaturesByIndex(0);
-  float temp = sensor.getTempCByIndex(0);
+  temperatureSensor.requestTemperaturesByIndex(0);
+  float temp = temperatureSensor.getTempCByIndex(0);
 
   // if we're not armed and under arming temperature
   // arm the alarm
-  if (!armed && temp < TEMP_ARM)
+  if (!armed && temp < TEMPERATURE_ARM)
   {
     armed = true;
+    // digitalWrite(PIN_INTERLOCK, true);
     #if DEBUG
-    Serial.print("ARMED");
+    Serial.println("ARMED");
     #endif
   }
 
   // over temperature
-  if (armed && temp >= TEMP_ALARM)
+  if (armed && temp >= TEMPERATURE_ALARM_HIGH)
   {
     digitalWrite(PIN_ALARM, (now / TICK_RATE) % 2);
+    // digitalWrite(PIN_INTERLOCK, false);
     #if DEBUG
-    Serial.print("ALARM");
+    Serial.println("ALARM HIGH");
     #endif
   }
-
-  // calculate how many coolers should be on
-  float levelTemp = TEMP_MIN + level * TEMP_INCREMENT;
-  if (temp > levelTemp + TEMP_HYSTERESIS)
-    ++level;
-  if (temp < levelTemp)
-    --level;
-
-  // turn them on (or off)
-  size_t i = 0;
-  for (; i < level; ++i)
+  else if (armed && temp <= TEMPERATURE_ALARM_LOW)
   {
-    // don't modify purging coolers
-    if (purge == i)
-      continue;
-    digitalWrite(PIN_COOLERS[i], 0); // active LOW
-    lastOn[i] = now;
-  }
-  for (; i < NUM_COOLERS; ++i)
-  {
-    // don't modify purging coolers
-    if (purge == i)
-      continue;
-    digitalWrite(PIN_COOLERS[i], 1);
-  }
-
-  if (purge == NUM_COOLERS)
-  {
-    size_t oldest = 0;
-    for (size_t i = 1; i < NUM_COOLERS; ++i)
-    {
-      if (lastOn[i] < lastOn[oldest])
-        oldest = i;
-    }
-
-    if (lastOn[oldest] > PURGE_INTERVAL)
-    {
-      purge = oldest;
-      lastOn[purge] = now;
-      digitalWrite(PIN_COOLERS[purge], 0);
-    }
+    digitalWrite(PIN_ALARM, (now / (TICK_RATE / 2)) % 2);
+    // digitalWrite(PIN_INTERLOCK, false);
+    #if DEBUG
+    Serial.println("ALARM LOW");
+    #endif
   }
   else
   {
-    if (lastOn[purge] + PURGE_TIME > now)
+    digitalWrite(PIN_ALARM, false);
+  }
+
+  // calculate how many coolers should be on
+  float levelTemp = TEMPERATURE_MINIMUM + level * TEMPERATURE_INCREMENT;
+  if (temp > levelTemp + TEMPERATURE_HYSTERESIS && level < NUM_COOLERS)
+  {
+    ++level;
+    turnCoolerOn(now);
+  }
+  if (temp < levelTemp && level > 0)
+  {
+    --level;
+    turnCoolerOff(now);
+  }
+
+  #if DEBUG
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(", level: ");
+  Serial.print(level);
+  Serial.print(" (T: ");
+  Serial.print(tail);
+  Serial.print(", H: ");
+  Serial.print(head);
+  Serial.println(")");
+  #endif
+
+  // see if we should move onto the next cooler as part of purging/limiting use
+  // if we're under temperature or flat out then there's nothing to do
+  if (level > 0 && level < NUM_COOLERS - 1)
+  {
+    const auto prev = (head + NUM_COOLERS - 1) % NUM_COOLERS;
+    if (lastOn[prev] + PURGE_TIME < now && lastOff[head] + PURGE_INTERVAL < now)
     {
-      lastOn[purge] = now;
-      digitalWrite(PIN_COOLERS[purge], 1);
-      purge = NUM_COOLERS;
+      #if DEBUG
+      Serial.print("Purge stepped to ");
+      Serial.println(head);
+      Serial.print("Tail last on ");
+      Serial.print(lastOn[tail]);
+      Serial.print(", head last off ");
+      Serial.println(lastOff[head]);
+      #endif
+      turnCoolerOn(now);
+      turnCoolerOff(now);
     }
   }
 }
